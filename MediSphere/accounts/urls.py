5329eb58255acba8e36e5973ce23ddc16298686e
imashu12 <imashu1210@gmail.com>
2025-02-26 19:00:51 +0530

    Delete MediSphere directory

--- MediSphere/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("signup/", views.signup_view, name="signup"),
-    path("login/",views.login_view,name='login'),
-    path('verify-otp/', views.verify_otp, name='verify_otp'),
-    path('logout/',views.logout_page,name='logout')
-]
